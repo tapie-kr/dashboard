4d102392a5610d54607021c99a7571be18d66255
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-21 16:09:13 +0900

    feat: add footer component with styling and positioning

--- src/app/page.css.ts
+++ src/app/page.css.ts
@@ -3,4 +3,11 @@ import { style } from '@vanilla-extract/css';
 
 export const base = style({
   background: colorVars.surface.default,
+  position: 'relative',
+});
+
+export const footer = style({
+  position: 'absolute',
+  bottom: 24,
+  color: colorVars.content.muted,
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,6 +36,18 @@ export default function IndexPage() {
         </VStack>
         <Button.Default leadingIcon={BrandIcon.GOOGLE}>Google로 TAPIE 로그인</Button.Default>
       </VStack>
+
+      <VStack
+        spacing={spacingVars.micro}
+        align={StackAlign.CENTER}
+        className={s.footer}
+      >
+        <Typo.Micro>© 2025 TAPIE. All rights reserved.</Typo.Micro>
+        <Typo.Mini>
+          로그인 시도 및 접속 기록은 모두 수집되며 비정상적인 활동 감지시 TAPIE는 대응을 위해 해당
+          기록을 활용할 수 있습니다.
+        </Typo.Mini>
+      </VStack>
     </HStack>
   );
 }
