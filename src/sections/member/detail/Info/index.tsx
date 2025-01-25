4da9c04c6c1be19cb132de140cd1acf035f62610
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-26 05:37:08 +0900

    feat: add MemberDetailInfoSection component for displaying member details and history

---
+++ src/sections/member/detail/Info/index.tsx
@@ -0,0 +1,323 @@
+'use client';
+
+import {
+  Badge,
+  BadgeSize,
+  BadgeTheme,
+  Button,
+  ButtonSize,
+  DataTable,
+  GlyphIcon,
+  HStack,
+  spacingVars,
+  StackJustify,
+  Typo,
+  VStack,
+  Weight,
+} from '@tapie-kr/inspire-react';
+import {
+  getSkillIcon,
+  type MemberInfoAwards,
+  type MemberInfoHistory,
+  type MemberInfoPortfolio,
+  type MemberInfoProfileLink,
+  type MemberInfoSkill,
+} from './shared';
+
+interface MemberDetailInfoSectionProps {
+  profileLink: MemberInfoProfileLink[];
+  awards: MemberInfoAwards[];
+  portfolio: MemberInfoPortfolio[];
+  skill: MemberInfoSkill[];
+  history: MemberInfoHistory[];
+}
+
+export default function MemberDetailInfoSection(props: MemberDetailInfoSectionProps) {
+  const { profileLink, awards, portfolio, skill, history } = props;
+
+  return (
+    <VStack spacing={spacingVars.jumbo}>
+      <InfoItem
+        title={'프로필 링크'}
+        buttonText={'링크 추가'}
+      >
+        <DataTable
+          showIndex
+          actions={[
+            {
+              icon: GlyphIcon.EDIT,
+              onClick: () => {},
+            },
+            {
+              icon: GlyphIcon.DELETE,
+              onClick: () => {},
+            },
+          ]}
+          columns={[
+            {
+              key: 'label',
+              label: '라벨',
+              width: 150,
+              isSortable: true,
+            },
+            {
+              key: 'link',
+              label: '링크',
+              width: 300,
+              isSortable: true,
+            },
+            {
+              key: 'icon',
+              label: '아이콘',
+              width: 180,
+              isSortable: true,
+              cell: (icon, _, data) => (
+                <Badge.Default
+                  leadingIcon={icon}
+                  label={data.label}
+                />
+              ),
+            },
+          ]}
+          data={profileLink}
+        />
+      </InfoItem>
+      <InfoItem
+        title={'수상실적'}
+        buttonText={'수상실적 추가'}
+      >
+        <DataTable
+          showIndex
+          actions={[
+            {
+              icon: GlyphIcon.EDIT,
+              onClick: () => {},
+            },
+            {
+              icon: GlyphIcon.DELETE,
+              onClick: () => {},
+            },
+          ]}
+          columns={[
+            {
+              key: 'contestName',
+              label: '대회 이름',
+              width: 250,
+              isSortable: true,
+            },
+            {
+              key: 'gradeLabel',
+              label: '등급 이름',
+              width: 130,
+              isSortable: true,
+            },
+            {
+              key: 'grade',
+              label: '등급',
+              width: 100,
+              isSortable: true,
+            },
+            {
+              key: 'memberCount',
+              label: '참여 인원',
+              width: 100,
+              isSortable: true,
+              cell: memberCount => `${memberCount}명`,
+            },
+          ]}
+          data={awards}
+        />
+      </InfoItem>
+      <InfoItem
+        title={'포트폴리오'}
+        buttonText={'포트폴리오 추가'}
+      >
+        <DataTable
+          showIndex
+          actions={[
+            {
+              icon: GlyphIcon.EDIT,
+              onClick: () => {},
+            },
+            {
+              icon: GlyphIcon.DELETE,
+              onClick: () => {},
+            },
+          ]}
+          columns={[
+            {
+              key: 'title',
+              label: '이름',
+              width: 150,
+              isSortable: true,
+            },
+            {
+              key: 'tags',
+              label: '태그',
+              width: 270,
+              isSortable: true,
+              cell: tags => (
+                <HStack spacing={spacingVars.tiny}>
+                  {tags.map((tag, index) => (
+                    <Badge.Default
+                      size={BadgeSize.SMALL}
+                      label={tag.name}
+                      leadingIcon={tag.icon}
+                      key={index}
+                    />
+                  ))}
+                </HStack>
+              ),
+            },
+            {
+              key: 'memberCount',
+              label: '참여 인원',
+              width: 100,
+              isSortable: true,
+              cell: viewCount => `${viewCount}명`,
+            },
+            {
+              key: 'viewCount',
+              label: '조회수',
+              width: 100,
+              isSortable: true,
+            },
+            {
+              key: 'downloadCount',
+              label: '다운로드 횟수',
+              width: 100,
+              isSortable: true,
+            },
+          ]}
+          data={portfolio}
+        />
+      </InfoItem>
+      <InfoItem
+        title={'기술'}
+        buttonText={'기술 추가'}
+      >
+        <DataTable
+          showIndex
+          actions={[
+            {
+              icon: GlyphIcon.EDIT,
+              onClick: () => {},
+            },
+            {
+              icon: GlyphIcon.DELETE,
+              onClick: () => {},
+            },
+          ]}
+          columns={[
+            {
+              key: 'name',
+              label: '이름',
+              width: 200,
+              isSortable: true,
+            },
+            {
+              key: 'type',
+              label: '종류',
+              width: 180,
+              isSortable: true,
+              cell: type => (
+                <Badge.Default
+                  leadingIcon={getSkillIcon(type)}
+                  label={type}
+                />
+              ),
+            },
+            {
+              key: 'icon',
+              label: '아이콘',
+              width: 100,
+              isSortable: true,
+              cell: (icon, _, data) => (
+                <Badge.Default
+                  leadingIcon={icon}
+                  label={data.name}
+                />
+              ),
+            },
+          ]}
+          data={skill}
+        />
+      </InfoItem>
+      <InfoItem
+        title={'연혁'}
+        buttonText={'연혁 추가'}
+      >
+        <DataTable
+          showIndex
+          actions={[
+            {
+              icon: GlyphIcon.EDIT,
+              onClick: () => {},
+            },
+            {
+              icon: GlyphIcon.DELETE,
+              onClick: () => {},
+            },
+          ]}
+          columns={[
+            {
+              key: 'label',
+              label: '라벨',
+              width: 300,
+              isSortable: true,
+            },
+            {
+              key: 'link',
+              label: '링크',
+              width: 200,
+              isSortable: true,
+            },
+            {
+              key: 'isImportant',
+              label: '중요',
+              width: 180,
+              isSortable: true,
+              cell: isImportant =>
+                isImportant && (
+                  <Badge.Default
+                    leadingIcon={GlyphIcon.ASTERISK}
+                    theme={BadgeTheme.RED}
+                    label={'중요'}
+                  />
+                ),
+            },
+          ]}
+          data={history}
+        />
+      </InfoItem>
+    </VStack>
+  );
+}
+
+interface InfoItemProps {
+  title: string;
+  buttonText: string;
+  children: React.ReactNode;
+}
+
+function InfoItem(props: InfoItemProps) {
+  const { title, buttonText, children } = props;
+
+  return (
+    <VStack spacing={spacingVars.mini}>
+      <HStack
+        justify={StackJustify.BETWEEN}
+        fullWidth
+      >
+        <Typo.Moderate weight={Weight.SEMIBOLD}>{title}</Typo.Moderate>
+        <Button.Default
+          size={ButtonSize.SMALL}
+          leadingIcon={GlyphIcon.ADD}
+        >
+          {buttonText}
+        </Button.Default>
+      </HStack>
+      {children}
+    </VStack>
+  );
+}
---
+++ src/sections/member/detail/Info/shared.ts
@@ -0,0 +1,56 @@
+import { GlyphIcon, type IconName } from '@tapie-kr/inspire-react';
+
+export interface MemberInfoProfileLink {
+  label: string;
+  link: string;
+  icon: IconName;
+}
+
+export interface MemberInfoAwards {
+  contestName: string;
+  gradeLabel: string;
+  grade: number;
+  memberCount: number;
+}
+
+interface Tag {
+  name: string;
+  icon: IconName;
+}
+
+export interface MemberInfoPortfolio {
+  title: string;
+  tags: Tag[];
+  memberCount: number;
+  viewCount: number;
+  downloadCount: number;
+}
+
+export enum Skill {
+  DEVELOPMENT = '개발',
+  MANAGEMENT = '관리',
+  ETC = '기타',
+}
+
+export const getSkillIcon = (skill: Skill): IconName => {
+  switch (skill) {
+    case Skill.DEVELOPMENT:
+      return GlyphIcon.CODE;
+    case Skill.MANAGEMENT:
+      return GlyphIcon.FLAG;
+    case Skill.ETC:
+      return GlyphIcon.EDIT;
+  }
+};
+
+export interface MemberInfoSkill {
+  name: string;
+  type: Skill;
+  icon: IconName;
+}
+
+export interface MemberInfoHistory {
+  label: string;
+  link: string;
+  isImportant: boolean;
+}
