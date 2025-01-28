3b30f78f99636f95ea7e45d9439cb0d40ca08c8c
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-29 05:46:50 +0900

    feat: add theme prop to delete button in AchievementDetailActionSection

--- src/sections/achievement/detail/Action/index.tsx
+++ src/sections/achievement/detail/Action/index.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonSize, GlyphIcon, HStack, spacingVars } from '@tapie-kr/inspire-react';
+import { Button, ButtonSize, GlyphIcon, HStack, spacingVars, Theme } from '@tapie-kr/inspire-react';
 
 export default function AchievementDetailActionSection() {
   return (
@@ -6,6 +6,7 @@ export default function AchievementDetailActionSection() {
       <Button.Default size={ButtonSize.SMALL}>변경사항 저장하기</Button.Default>
       <Button.Default
         size={ButtonSize.SMALL}
+        theme={Theme.RED}
         leadingIcon={GlyphIcon.DELETE}
       >
         수상실적 삭제하기
