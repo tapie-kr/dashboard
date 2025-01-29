084c26d76d2d569847062a4318afcf45f72fdcf5
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-29 10:41:46 +0900

    feat: add ButtonVariant to AchievementDetail and ApplicationDetail action sections

--- src/sections/achievement/detail/Action/index.tsx
+++ src/sections/achievement/detail/Action/index.tsx
@@ -1,10 +1,19 @@
-import { Button, ButtonSize, GlyphIcon, HStack, spacingVars, Theme } from '@tapie-kr/inspire-react';
+import {
+  Button,
+  ButtonSize,
+  ButtonVariant,
+  GlyphIcon,
+  HStack,
+  spacingVars,
+  Theme,
+} from '@tapie-kr/inspire-react';
 
 export default function AchievementDetailActionSection() {
   return (
     <HStack spacing={spacingVars.petite}>
       <Button.Default size={ButtonSize.SMALL}>변경사항 저장하기</Button.Default>
       <Button.Default
+        variant={ButtonVariant.SECONDARY}
         size={ButtonSize.SMALL}
         theme={Theme.RED}
         leadingIcon={GlyphIcon.DELETE}
--- src/sections/application/detail/Action/index.tsx
+++ src/sections/application/detail/Action/index.tsx
@@ -1,10 +1,19 @@
-import { Button, ButtonSize, GlyphIcon, HStack, spacingVars, Theme } from '@tapie-kr/inspire-react';
+import {
+  Button,
+  ButtonSize,
+  ButtonVariant,
+  GlyphIcon,
+  HStack,
+  spacingVars,
+  Theme,
+} from '@tapie-kr/inspire-react';
 
 export default function ApplicationDetailActionSection() {
   return (
     <HStack spacing={spacingVars.petite}>
       <Button.Default size={ButtonSize.SMALL}>합격 처리</Button.Default>
       <Button.Default
+        variant={ButtonVariant.SECONDARY}
         size={ButtonSize.SMALL}
         theme={Theme.RED}
         leadingIcon={GlyphIcon.DELETE}
