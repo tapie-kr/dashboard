1abfefb12a5f2927ba45d69f05bb4b495139c0c9
iamfiro <appturbo102@gmail.com>
2025-01-30 17:43:53 +0900

    feat: add footer section to dashboard auth page with copyright and activity notice

--- src/app/(dashboard)/page.css.ts
+++ src/app/(dashboard)/page.css.ts
@@ -1,5 +1,12 @@
+import { colorVars } from "@tapie-kr/inspire-react";
 import { style } from "@vanilla-extract/css";
 
 export const container = style({
     width: 300,
+});
+
+export const footer = style({
+  position: 'absolute',
+  bottom: 24,
+  color: colorVars.content.muted,
 });
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,4 @@
-import * as s from './page.css'
+import * as s from './page.css';
 
 import {
   BrandIcon,
@@ -34,7 +34,22 @@ export default function IndexPage() {
           />
           <Typo.Medium weight={Weight.BOLD}>관리 시스템</Typo.Medium>
         </VStack>
-        <Button.Default fullWidth leadingIcon={BrandIcon.GOOGLE}>Google로 TAPIE 로그인</Button.Default>
+        <Button.Default
+          fullWidth
+          leadingIcon={BrandIcon.GOOGLE}
+        >
+          Google로 TAPIE 로그인
+        </Button.Default>
+      </VStack>
+      <VStack
+        spacing={spacingVars.micro}
+        className={s.footer}
+      >
+        <Typo.Micro>© 2025 TAPIE. All rights reserved.</Typo.Micro>
+        <Typo.Mini>
+          로그인 시도 및 접속 기록은 모두 수집되며 비정상적인 활동 감지시 TAPIE는 대응을 위해 해당
+          기록을 활용할 수 있습니다.
+        </Typo.Mini>
       </VStack>
     </HStack>
   );
