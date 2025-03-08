7e201b94c747a1971f372117f5ed3ad616b0ee9f
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-03-09 04:14:41 +0900

    feat: update withTAPIEDashboardConfig to accept type parameter for dynamic API_HOSTNAME configuration

--- packages/admin/next.config.mjs
+++ packages/admin/next.config.mjs
@@ -1,3 +1,3 @@
 import { withTAPIEDashboardConfig } from '@tapie-kr/dashboard-shared/configs/nextjs.mjs';
 
-export default withTAPIEDashboardConfig({ env: { API_HOSTNAME: process.env.API_HOSTNAME || 'http://localhost:6678' } });
+export default withTAPIEDashboardConfig('admin');
--- packages/shared/configs/nextjs.mjs
+++ packages/shared/configs/nextjs.mjs
@@ -2,16 +2,14 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 
 /**
  * TAPIE 대시보드 웹 프로젝트를 위한 Next.js 설정을 반환합니다.
- * @param {import('next').NextConfig} config
+ * @param {string} type
  * @returns
  */
-export function withTAPIEDashboardConfig(config = {}) {
+export function withTAPIEDashboardConfig(type) {
   const withVanillaExtract = createVanillaExtractPlugin({ identifiers: 'short' });
 
   /** @type {import('next').NextConfig} */
   const nextConfig = {
-    ...config,
-
     webpack(config) {
       const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.('.svg'));
 
@@ -38,9 +36,8 @@ export function withTAPIEDashboardConfig(config = {}) {
       NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://tapie-api-dev.vport.dev',
       AUTH_SERVICE:        'website',
       AUTH_URL:            process.env.AUTH_URL || 'http://localhost:9876',
-      API_HOSTNAME:        process.env.API_HOSTNAME || 'http://localhost:8765',
+      API_HOSTNAME:        type === 'admin' ? process.env.API_HOSTNAME || (process.env.NODE_ENV === 'production' ? 'https://api.tapie.kr' : 'http://localhost:6678') : process.env.API_HOSTNAME || (process.env.NODE_ENV === 'production' ? 'https://api.tapie.kr' : 'http://localhost:7000'),
       API_VERSION:         process.env.API_VERSION || 'api/v1',
-      ...config.env,
     },
 
     transpilePackages: ['@tapie-kr/api-client'],
